3b0c75b5c00ed311e09ea86f8ac7578a4968e454
HoangTrongDao <daohtkh023@gmail.com>
2026-08-16 08:14:12 +0700

    Ac

--- PY01014.py
+++ PY01014.py
@@ -1,1 +1,14 @@
+def lietke(a,k,n):
+    kq = ((a+1)+k-1) // k*k
+    c = []
+    for sum in range(kq,n+1,k):
+        b = sum - a
+        c.append(b)
+    return c
+
 a,k,n = list(map(int,input().split()))
+list_kq=lietke(a,k,n)
+if list_kq:
+    print(*list_kq)
+else:
+    print(-1)
