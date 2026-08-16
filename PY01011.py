1fcc70a84fff5a14c35c2c3367acc598f1a6a4ce
HoangTrongDao <daohtkh023@gmail.com>
2026-08-16 08:46:40 +0700

    Ac 11, dung try except

--- PY01011.py
+++ PY01011.py
@@ -1,15 +1,29 @@
-def dep(n):
-    s = str(n)
-    return (
-        len(s) % 2 == 0
-        and all(c in "02468" for c in s)
-        and s == s[::-1]
-    )
-t = int(input())
-while t:
-    n = int(input())
-    for i in range(1, n):
-        if dep(i):
-            print(i, end=" ")
-    print()
-    t -= 1
+import sys
+
+list_so = []
+queue = ["2", "4", "6", "8"]
+
+while queue:
+    nua_dau = queue.pop(0)
+    s_dx = int(nua_dau + nua_dau[::-1])
+
+    if s_dx >= 1000000:
+        continue
+    list_so.append(s_dx)
+    if len(nua_dau) < 3:
+        for so in ["0", "2", "4", "6", "8"]:
+            queue.append(nua_dau + so)
+
+list_so.sort()
+try:
+    t = int(input().strip())
+    for _ in range(t):
+        n = int(input().strip())
+        for x in list_so:
+            if x < n:
+                print(x, end=" ")
+            else:
+                break
+        print()
+except EOFError:
+    pass
